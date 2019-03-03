b13bf75e204e17868f5dd07d7d0509784261899b
SssWind <sam.zj.shao@gmail.com>
2019-03-03 10:53:37 -0500

    Chrome V1.0.1

--- Quest_Raspberry_Chrome/js/page/GRDE.UW_SSR_SSENRL_TERM.js
+++ Quest_Raspberry_Chrome/js/page/GRDE.UW_SSR_SSENRL_TERM.js
@@ -1,5 +1,28 @@
 function initGrdeUwSsrSsenrlTerm() {
 
+    function optimizePage() {
+
+        var flag = $('#win0divPSPAGECONTAINER');
+        if (flag.attr('data-optimized') == '1' && optimizeLocked) return;
+        flag.attr('data-optimized', '1');
+        optimizeLocked = true;
+        setTimeout(function () {
+            optimizeLocked = false;
+        }, 1000);
+
+        $('tr[id^="trSSR_DUMMY_RECV1$0_row"]').on('click', function () {
+            $(this).find('input[id^="SSR_DUMMY_RECV1$sels$"][class="PSRADIOBUTTON"]').prop('checked', true);
+        });
+    }
+
+    var optimizeLocked = false;
+    optimizePage();
+
+    $('#win0divPAGECONTAINER').on('DOMSubtreeModified', function () {
+        setTimeout(function () {
+            optimizePage();
+        }, 5);
+    });
 }
 
 initGrdeUwSsrSsenrlTerm();
