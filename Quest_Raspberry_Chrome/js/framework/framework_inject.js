952c4928794c3e2b3f5ffad015f5d26f187b075d
SssWind <sam.zj.shao@gmail.com>
2019-03-05 22:10:14 -0500

    Chrome V1.0.2

--- Quest_Raspberry_Chrome/js/framework/framework_inject.js
+++ Quest_Raspberry_Chrome/js/framework/framework_inject.js
@@ -3,10 +3,10 @@ var rbApp = {
     keepLogin: function () {
         setInterval(function () {
             var xhttp1 = new XMLHttpRequest();
-            xhttp1.open("GET", "'/psc/SS/ACADEMIC/SA/c/?cmd=ping", true);
+            xhttp1.open("GET", "/psc/SS/ACADEMIC/SA/c/?cmd=ping", true);
             xhttp1.send();
             var xhttp2 = new XMLHttpRequest();
-            xhttp2.open("GET", "'/psc/SS/ACADEMIC/SA/c/?cmd=resettimeout", true);
+            xhttp2.open("GET", "/psc/SS/ACADEMIC/SA/c/?cmd=resettimeout", true);
             xhttp2.send();
 
             if (typeof resetAutoSave === typeof function () {
